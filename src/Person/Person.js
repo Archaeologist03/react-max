dc994bd2f0d6bfa6d242b8ae4f36ea7789785c88
Ilija <omniiknight@hotmail.com>
2018-11-21 23:50:49 +0100

    Adding end of styling compoment elements - course files - my config failed

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,17 +1,14 @@
-import React from "react";
-// import Radium from "radium";
+import React from 'react';
 
+import classes from './Person.css';
 
-import './Person.css';
-
-const person = (props) => {
-
-
+const person = ( props ) => {
     return (
-        <div className="Person">
-            <p onClick={props.click}>I am {props.name} and i am {props.age}yo. {props.children}</p>
+        <div className={classes.Person}>
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
+            <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
-        </div>  
+        </div>
     )
 };
 
